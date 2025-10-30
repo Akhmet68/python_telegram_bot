d4e00d8bf09527f78d8c307277fa068e6dc17c13
Akhmet68 <zhalgasbaiakhmet@gmail.com>
2025-10-31 02:57:28 +0500

    main

--- bot.py
+++ bot.py
@@ -2,21 +2,21 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from config import API_TOKEN
 from database import init_db
-from handlers import start, language, level, lessons, register, topics
+from handlers import start, language, level, lessons, register
 
 async def main():
     await init_db()
+
     bot = Bot(token=API_TOKEN)
     dp = Dispatcher()
-
     dp.include_router(start.router)
     dp.include_router(register.router)
     dp.include_router(language.router)
     dp.include_router(level.router)
     dp.include_router(lessons.router)
-    dp.include_router(topics.router)
 
-    print("Бот запущен и готов к работе")
+    print("Бот запущен и готов к работе!")
+
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
